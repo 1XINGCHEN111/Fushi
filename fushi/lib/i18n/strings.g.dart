dbc683a710bd99582cba9b84a642eacda30662fb
wrds <vw6cnhd9f7@privaterelay.appleid.com>
2026-08-17 13:32:21 +0800

    chore(i18n): regenerate strings.g.dart after batch merge

--- fushi/lib/i18n/strings.g.dart
+++ fushi/lib/i18n/strings.g.dart
@@ -3,7 +3,7 @@
 /// Locales: 17
 /// Strings: 59041 (3473 per locale)
 ///
-/// Built on 2026-08-17 at 05:30 UTC
+/// Built on 2026-08-17 at 05:31 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
